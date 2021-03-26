61726737db3eb62f8bea52302306e149b89df903
Matthew Horne <matthew.horne@fifteen.co.uk>
2021-03-26 19:50:54 +0000

    test updates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import RegisterForm from './containers/RegisterForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders register form', () => {
+  const mockFn = jest.fn()
+  const { getByTestId } = render(<RegisterForm onSubmit={mockFn} />);
+  const form = getByTestId('register-form');
+  expect(form).toBeInTheDocument();
 });
--- src/containers/__tests__/registerForm.test.tsx
+++ src/containers/__tests__/registerForm.test.tsx
@@ -3,17 +3,13 @@ import { render, fireEvent, act } from '@testing-library/react'
 
 import RegisterForm from '../RegisterForm'
 
-it('renders without crashing', () => {
-  const onSubmit = jest.fn()
-  render(<RegisterForm onSubmit={onSubmit} />)
-})
 
 it('calls onSubmit with username, email, password and terms checked when submitted', async () => {
   const promise = Promise.resolve()
   const mockSubmit = jest.fn(() => promise)
-  const { queryByTestId } = render(<RegisterForm onSubmit={mockSubmit} />)
+  const { getByTestId } = render(<RegisterForm onSubmit={mockSubmit} />)
 
-  fireEvent.submit(queryByTestId('register-form'))
+  fireEvent.submit(getByTestId('register-form'))
 
   await act(() => promise)
 
